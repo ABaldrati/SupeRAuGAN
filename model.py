6f86825eddb5644aa615e25980e9e2c46645e3f7
Alberto Baldrati <a_baldrati@hotmail.it>
2021-01-13 11:14:08 +0100

    feat(model): add `UpsampleBlock` module

--- model.py
+++ model.py
@@ -22,3 +22,17 @@ class ResidualBlock(nn.Module):
         y = self.bn2(y)
 
         return x + y
+
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels, up_scale):
+        super(UpsampleBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, in_channels * up_scale ** 2, kernel_size=3, padding=1)
+        self.pixel_shuffle = nn.PixelShuffle(up_scale)
+        self.prelu = nn.PReLU()
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.pixel_shuffle(x)
+        x = self.prelu(x)
+        return x
