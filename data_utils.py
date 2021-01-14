dabf438b1d584acfc23f5d6033dd9313ef68e19d
Alberto Baldrati <a_baldrati@hotmail.it>
2021-01-14 14:37:34 +0100

    refactor(data_utils): rename `CROP_SIZE` to `PATCH_SIZE`

--- data_utils.py
+++ data_utils.py
@@ -41,12 +41,12 @@ def display_transform():
 
 
 class TrainDatasetFromFolder(Dataset):
-    def __init__(self, dataset_dir, crop_size, upscale_factor):
+    def __init__(self, dataset_dir, patch_size, upscale_factor):
         super(TrainDatasetFromFolder, self).__init__()
         self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
-        crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
-        self.hr_transform = train_hr_transform(crop_size)
-        self.lr_transform = train_lr_transform(crop_size, upscale_factor)
+        patch_size = calculate_valid_crop_size(patch_size, upscale_factor)
+        self.hr_transform = train_hr_transform(patch_size)
+        self.lr_transform = train_lr_transform(patch_size, upscale_factor)
 
     def __getitem__(self, index):
         hr_image = self.hr_transform(Image.open(self.image_filenames[index]))
