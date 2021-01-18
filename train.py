d2cfbd6016a2bef97a6afac86a9235677ed97acf
Alberto Baldrati <a_baldrati@hotmail.it>
2021-01-18 16:28:07 +0100

    perf(train): use cudnn benchmark

--- train.py
+++ train.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
     d_net = Discriminator()
 
     if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = True
         g_net.cuda()
         d_net.cuda()
 
