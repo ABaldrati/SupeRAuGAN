798faf3dd7a47e355bf3c2485efad080a0e1cca0
Giovanni Berti <dev.giovanniberti@gmail.com>
2021-02-03 18:35:36 +0100

    refactor(model): remove batch norm layers

--- model.py
+++ model.py
@@ -9,17 +9,13 @@ class ResidualBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
         self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(channels)
         self.prelu = nn.PReLU()
         self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
-        self.bn2 = nn.BatchNorm2d(channels)
 
     def forward(self, x):
         y = self.conv1(x)
-        y = self.bn1(y)
         y = self.prelu(y)
         y = self.conv2(y)
-        y = self.bn2(y)
 
         return x + y
 
